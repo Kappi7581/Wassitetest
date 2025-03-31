2f282d1a27fc84573b9b2e294079932ee51c1fc9
Kappi7581 <Kappi7581@outlook.com>
2025-03-31 10:57:51 +0300

    Add files via upload

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="text-zinc-400 text-center py-4">
+      <div className="container mx-auto">
+        <p className="text-sm">Made by <span className="text-white underline"><a href="t.me/uhuvi">İnsomiaWas</a></span> with ❤️</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
